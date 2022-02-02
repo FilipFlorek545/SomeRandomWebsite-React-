e1ed77bb9d1f02c66dc64b8bf47ddbcd882b9e78
Filip Florek <florson467@icloud.com>
2022-02-02 20:24:11 +0100

    Cleaned the code

--- src/App.js
+++ src/App.js
@@ -6,32 +6,30 @@ return(
         <div id="top">
             <Link className='companyName' adress='#'>SomeRandomCompany</Link>
             <div id="top_sidebar">
-                <Link className='top_nav' adress='#'>About Us</Link>
-                <Link className='top_nav' adress='#'>Our Offer</Link>
+                <Link className='top_nav' adress='#container_2'>About Us</Link>
+                <Link className='top_nav' adress='#container_3'>Our Offer</Link>
                 <Link className='top_nav' adress='#'>Contact</Link>
             </div>
         </div>
-        <div className="container no_1">
+        <div id="container_1">
             <Heading>Our company strives to offer you products of the best quality</Heading>
             <SmallerHeading>Don't take our word for it, check!</SmallerHeading>
             <Button>CHECK OUR DEALS</Button>
         </div>
-        <div className="container no_2">
-            <Heading className='specialist_heading'>Our Specialists</Heading>
-            <div className="specialist_box">
-                <Specialist specialist_class='specialist_bubble nr_1'/>
-                <Paragraph className='specialist_desc desc_1'>
+        <div id="container_2">
+            <Heading className='spec_heading'>Our Specialists</Heading>
+                <Specialist specialist_class='spec_bubble bubble_1'/>
+                <Paragraph className='specialist spec_1'>
                     <b>Name Surname</b><br />
                     <br />
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit minus similique mollitia explicabo. Ratione quod delectus repellat rem cumque corporis saepe praesentium beatae mollitia aperiam!</Paragraph>
-                <Paragraph className='specialist_desc desc_2'>
+                <Paragraph className='specialist spec_2'>
                     <b>Name Surname</b><br />
                     <br />
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptas cum dolor perspiciatis blanditiis nostrum repellat vitae quo, quis amet ad veritatis id! Doloremque, cum.</Paragraph>
-                <Specialist specialist_class='specialist_bubble nr_2'/>
-            </div>
+                <Specialist specialist_class='spec_bubble bubble_2'/>
         </div>
-        <div className="container no_3">
+        <div id="container_3">
             <Heading className='heading_no_3'>What can our company do for you?</Heading>
             <div className="servicebox">
                 <Service>Service</Service>
